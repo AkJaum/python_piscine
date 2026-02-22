94b5263ae57e07b1f8b6f3e85fc70c52a7e372e1
Joao Gabriel Neris Ferreira <tbpjaum@outlook.com>
2026-02-22 19:56:40 -0300

    commit das novas listas

---
+++ Module 03/ex1/ft_score_analytics.py
@@ -0,0 +1,46 @@
+import sys
+
+class ScoreManager():
+    def __init__(self):
+        self.scores = []
+        self.total_players = 0
+        self.total_score = 0
+        self.average_score = 0
+
+    def convert_to_list(self):
+        i = 1
+        while i < len(sys.argv):
+            try:
+                number = int(sys.argv[i])
+                self.scores.append(number)
+            except ValueError as Error:
+                print("Error:", Error)
+            i += 1
+    
+    def calculate_stats(self):
+        self.total_players = len(self.scores)
+        if self.total_players == 0:
+            return
+        self.total_score = sum(self.scores)
+        self.average_score = self.total_score / self.total_players
+
+if __name__ == "__main__":
+    print("=== Player Score Analytics ===")
+
+    if len(sys.argv) == 1:
+        print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+    else:
+        manager = ScoreManager()
+        manager.convert_to_list()
+        manager.calculate_stats()
+
+        if manager.total_players == 0:
+            print("No valid scores provided.")
+        else:
+            print("Scores processed:", manager.scores)
+            print("Total players:", manager.total_players)
+            print("Total score:", manager.total_score)
+            print("Average score:", manager.average_score)
+            print("High score:", max(manager.scores))
+            print("Low score:", min(manager.scores))
+            print("Score range:", max(manager.scores) - min(manager.scores))
---
+++ Module 03/ex2/ft_coordinate_system.py
@@ -0,0 +1,46 @@
+import math
+
+def parse_coords(coord_str):
+    coords = coord_str.split(",")
+    try:
+        x = int(coords[0])
+        y = int(coords[1])
+        z = int(coords[2])
+        pos = (x, y, z)
+        return pos
+    except ValueError as Error:
+        print("Error parsing coordinates:", Error)
+        print(f"Error details - Type: {type(Error).__name__}, Args: {Error.args}\n")
+    return None
+
+
+def create_pos(x, y, z):
+    pos = (x, y, z)
+    print("Position created:", pos)
+    return pos
+
+
+def calculate_distance(src, dest):
+    x1, y1, z1 = src
+    x2, y2, z2 = dest
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    print(f"Distance between {src} and {dest}: {distance:.2f}\n")
+
+
+if __name__ == "__main__":
+    print("=== Game Coordinate System ===\n")
+    origin = (0, 0, 0)
+    player = create_pos(10, 20, 5)
+    calculate_distance(origin, player)
+    coord_str = "3,4,0"
+    print(f'Parinsg coordinates: "{coord_str}"')
+    coord = parse_coords(coord_str)
+    print(f"Parsed position: {coord}")
+    if coord is not None:
+        calculate_distance(origin, coord)
+    print(f'Parsing invalid coordniates: "abc,def,ghi"')
+    parse_coords("abc,def,ghi")
+    print("Unpacking demonstration:")
+    x, y, z = coord
+    print(f"Player at x={x}, y={y}, z={z}")
+    print(f"Coordinates: X={x}, Y={y}, Z={z}")
