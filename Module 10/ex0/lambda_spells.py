a3dd61ddaecda873af7824e795109ca36e523a37
jneris-d <tbpjaum@outlook.com>
2026-05-14 17:54:52 -0300

    Começando a lista 10

---
+++ Module 10/ex0/lambda_spells.py
@@ -0,0 +1,14 @@
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    return sorted(artifacts, key=lambda x: x['power'], reverse=True)
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    return list(filter(lambda x: x['power'] >= min_power, mages))
+
+
+def spell_transformer(spells: list[dict]) -> list[dict]:
+    pass
+
+
+def mage_stats(mages: list[dict]) -> list[dict]:
+    pass
